f8e95fe641df45b42dd352b815049c86600bd1e8
Ajay <ajayrajnegi111@gmail.com>
2025-11-20 11:09:51 +0530

    Server components fetch data and hydration is done

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -3,17 +3,21 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import superjson from "superjson";
+
+// Creates a QueryClient
 export function makeQueryClient() {
+  // Creates an QueryClient instances which manages data-fetching, caching, synchronization and state-management
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 30 * 1000,
+        staleTime: 30 * 1000, // Query's data is considered fresh => 30s
       },
       dehydrate: {
         serializeData: superjson.serialize,
+        // Specifies which queries should have their data included in the HTML payload
         shouldDehydrateQuery: (query) =>
           defaultShouldDehydrateQuery(query) ||
-          query.state.status === "pending",
+          query.state.status === "pending", // Forces the queries to dehydrate which are still in progress
       },
       hydrate: {
         deserializeData: superjson.deserialize,
