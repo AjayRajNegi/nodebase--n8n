4f64ddd7d76e382be5a2266b80aa3dfe291a2d41
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-04 16:53:21 +0530

    Added execute procedure.

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,68 +1,12 @@
 import { inngest } from "@/inngest/client";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { generateText } from "ai";
-import { createOpenAI } from "@ai-sdk/openai";
-import { createAnthropic } from "@ai-sdk/anthropic";
 
-// Creates instances of each model
-const google = createGoogleGenerativeAI();
-const openai = createOpenAI();
-const anthropic = createAnthropic();
-
-export const execute = inngest.createFunction(
+export const executeWorkflow = inngest.createFunction(
   // Unique identifier
-  { id: "execute-ai" },
+  { id: "execute-workflow" },
   // Event that triggers the function
-  { event: "execute/ai" },
+  { event: "workflows/execute.workflow" },
   // Actual function logic
   async ({ event, step }) => {
-    await step.sleep("pretend", "5s");
-
-    const { steps: geminiSteps } = await step.ai.wrap(
-      "gemini-generate-text",
-      generateText,
-      {
-        model: google("gemini-2.5-flash"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2 +2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
-        },
-      }
-    );
-
-    const { steps: openaiSteps } = await step.ai.wrap(
-      "openai-generate-text",
-      generateText,
-      {
-        model: openai("gpt-4"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2 +2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
-        },
-      }
-    );
-
-    const { steps: anthropicSteps } = await step.ai.wrap(
-      "anthropic-generate-text",
-      generateText,
-      {
-        model: anthropic("claude-sonnet-4-0"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2 +2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
-        },
-      }
-    );
-
-    return { geminiSteps, openaiSteps, anthropicSteps };
+    await step.sleep("test", "5s");
   }
 );
