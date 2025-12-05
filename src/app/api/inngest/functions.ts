eac0bc79bc167031dbbbcbe3bc082f68f5390969
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-05 23:20:19 +0530

    Types for executor registry

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,4 +1,7 @@
+import { getExecutor } from "@/app/features/executions/lib/executor-registry";
+import { NodeType } from "@/generated/prisma";
 import { inngest } from "@/inngest/client";
+import { topologicalSort } from "@/inngest/utils";
 import prisma from "@/lib/db";
 import { NonRetriableError } from "inngest";
 
@@ -14,7 +17,7 @@ export const executeWorkflow = inngest.createFunction(
       throw new NonRetriableError("Workflow ID is missing.");
     }
 
-    const nodes = await step.run("prepare-workflow", async () => {
+    const sortedNodes = await step.run("prepare-workflow", async () => {
       const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: workflowId },
         include: {
@@ -23,9 +26,23 @@ export const executeWorkflow = inngest.createFunction(
         },
       });
 
-      return workflow;
+      return topologicalSort(workflow.nodes, workflow.connections);
     });
 
-    return { nodes };
+    // Initialize context with any initial data from the trigger
+    let context = event.data.initialData || {};
+
+    // Execute each node
+    for (const node of sortedNodes) {
+      const executor = getExecutor(node.type as NodeType);
+      context = await executor({
+        data: node.data as Record<string, unknown>;
+        nodeId: node.id;
+        context,
+        step
+      }) 
+    }
+
+    return { sortedNodes };
   }
 );
